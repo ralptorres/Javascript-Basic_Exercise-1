a72759e9949e34020aba852662b4737f45d8dbf0
ralptorres <ralp.torres@gmail.com>
2024-11-21 20:36:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+alert("Good morning");
+alert("Ralp");
+10 + 5;
+20 - 5;
+2 + 2 - 5;
+
+document.body.innerHTML = "Good morning";
+document.body.innerHTML = "Ralp";
+
+let tShirt = 10;
+let socks = 8;
+let dinnerPlates = 20;
+
+total = tShirt + socks + dinnerPlates;
+
+alert(`Total: $${total}`);
+
+let bankAccount = 100;
+let lunch = 20;
+let dinner = 50;
+let earn = 200;
+
+totalMoney = bankAccount - (lunch + dinner) + earn;
+
+alert(`Total money: $${totalMoney}`);
